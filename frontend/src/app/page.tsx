99a1e0aacd9cb83f24d55d57875e63c7c591c217
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-09-25 16:23:58 +0200

    starting the revamping of the home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Home/Hero";
-import SearchBar from "@/components/Home/SearchBar";
 import FeaturedContent from "@/components/Home/FeaturedContent";
+import { useState } from "react";
+import Sidebar from "@/components/Header/Sidebar";
 
 export default function Home() {
+  const [mainMargin, setMainMargin] = useState(64);
   return (
     <main className="bg-[#002440]">
-      <Header />
-      <Hero />
-      <SearchBar />
-      <FeaturedContent />
+      <Sidebar setMainMargin={setMainMargin} />
+      <div
+        className="flex-1 transition-all duration-300"
+        style={{ marginLeft: `${mainMargin}px` }}
+      >
+        <Hero />
+        <FeaturedContent />
+      </div>
     </main>
   );
 }
--- frontend/src/components/Home/Hero.tsx
+++ frontend/src/components/Home/Hero.tsx
@@ -1,14 +1,42 @@
+"use client";
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import SearchBar from "@/components/Home/SearchBar";
 
 export default function Hero() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <div className="relative h-[50vh] sm:h-[30vh] md:h-[40vh] lg:h-[40vh] xl:h-[50vh]">
-      <div className="absolute inset-0 bg-[#002440] flex flex-col justify-center items-center text-center">
-        <h1 className="text-5xl font-bold mb-4">Welcome to WowPerf</h1>
-        <p className="text-xl mb-8">
-          Explore characters, equipment, and talents to stay at the state of the
-          art.
-        </p>
+    <div className="relative min-h-screen w-full overflow-hidden">
+      {isMounted && (
+        <Image
+          src="/homepage.avif"
+          alt="World of Warcraft Castle"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+          className="filter brightness-50"
+        />
+      )}
+      <div className="absolute inset-0 flex flex-col items-center justify-center p-4">
+        <div className="text-center text-white z-10 max-w-4xl">
+          <h1 className="text-5xl md:text-7xl font-bold mb-4 text-left">
+            Elevate your WoW experience
+          </h1>
+          <p className="text-lg md:text-xl mb-8 text-gray-300 text-left">
+            Explore characters equipments, talents, mythic + and raids
+            progression to stay at the state of the art in World of Warcraft.
+          </p>
+          <div className="w-full max-w-xl">
+            <SearchBar />
+          </div>
+        </div>
       </div>
     </div>
   );
